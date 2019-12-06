fea880d7be54cc588271e0971c337f9f74e26c09
weijhfly <1241968795@qq.com>
2019-12-06 10:14:52 +0800

    修改层级

--- Night-Mode-Firfox/js/content.js
+++ Night-Mode-Firfox/js/content.js
@@ -20,7 +20,7 @@ browser.storage.onChanged.addListener(function(obj){
 
 function setStyle(obj){
   var mask = document.querySelector("#night-mask"),
-      css = 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;z-index: 999999;pointer-events: none;'+
+      css = 'position: fixed;top: 0;left: 0;width: 100%;height: 100%;z-index: 2147483647;pointer-events: none;'+
       'mix-blend-mode: multiply;transition: opacity 0.1s ease 0s;opacity:$opa;display:$dis;background: $bac;';
 
   css = css.replace('$opa',obj.range/100).replace('$dis',obj.on? 'block':'none').replace('$bac',obj.color);
